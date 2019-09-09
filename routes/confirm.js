e83127b460e4ec40c6e3c148058bb160459e2a4a
nesso mohamed <nessomohamed555@gmail.com>
2019-09-10 01:12:31 +0200

    test

--- routes/confirm.js
+++ routes/confirm.js
@@ -18,7 +18,7 @@ const express = require('express'),
          .then(user => user.updateOne({confirmed: true})
          )
          setTimeout(() => {
-          res.redirect( rocess.env.NODE_ENV + '/confirmed' ||'/confirmed')
+          res.redirect( process.env.NODE_ENV + '/confirmed' ||'/confirmed')
          }, 2000);
          
 
