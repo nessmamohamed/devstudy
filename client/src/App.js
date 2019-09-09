10ab8d950845f5ada65fb8d03684c9796028cce6
nesso mohamed <nessomohamed555@gmail.com>
2019-09-09 22:36:07 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import Header from './components/header'
+import Home from './components/pages/home'
+import About from  './components/pages/about'
+import confirmed from  './components/confirmed'
+import Social from './components/social'
+import Store from './components/pages/store'
+import Climping from './components/pages/climpingSim'
+import Chat from './components/chat'
+import { BrowserRouter as Router , Route} from 'react-router-dom'
+import './App.css'
+import LoginRegister from './components/pages/login'
+import store from './store'
+import {Provider} from 'react-redux'
+import {loadUser} from './actions/authActions'
 
-function App() {
+
+class App extends React.Component {
+    
+  componentDidMount(){
+    store.dispatch(loadUser())
+  }
+ 
+
+   
+  
+  render(){
+  
+
+
+
   return (
+     
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+      <Router>
+      <Header/>
+  
+     <Route exact path='/' component={Home}/>
+      <Route  path="/about" component={About}/>
+      <Route path="/store" component={Store}/>
+      <Route path = '/climpingSim' component={Climping}/>
+      <Route path= '/loginOrRegister' component={LoginRegister}/>
+      <Route path='/social' component={Social}/>
+      <Route path='/confirmed' component={confirmed}/>
+      </Router>
+       
+       
+       <Chat/>
+
+      
     </div>
+    </Provider>
   );
+  }
 }
+
+
 
 export default App;
