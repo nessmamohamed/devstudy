8dd31e998a8070a6275c876cee28e2dcbca9d26d
nesso mohamed <nessomohamed555@gmail.com>
2020-02-28 19:31:44 +0200

    Updates

---
+++ precache-manifest.7030ec88d54bad9c350a65e0bba19c25.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171fb900e0a12bc0dd8ad5df81e4f334",
+    "url": "/devstudy/index.html"
+  },
+  {
+    "revision": "53a0cded9243bf54abbd",
+    "url": "/devstudy/static/css/2.0ca4e8b5.chunk.css"
+  },
+  {
+    "revision": "9b0c3869ba41d25f5269",
+    "url": "/devstudy/static/css/main.991a5080.chunk.css"
+  },
+  {
+    "revision": "53a0cded9243bf54abbd",
+    "url": "/devstudy/static/js/2.aea13d24.chunk.js"
+  },
+  {
+    "revision": "a60898215175f3e8912ea40551904c19",
+    "url": "/devstudy/static/js/2.aea13d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b0c3869ba41d25f5269",
+    "url": "/devstudy/static/js/main.b067d592.chunk.js"
+  },
+  {
+    "revision": "913b47d0db43e3630601",
+    "url": "/devstudy/static/js/runtime-main.47b48313.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/devstudy/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/devstudy/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/devstudy/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/devstudy/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/devstudy/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/devstudy/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/devstudy/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/devstudy/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/devstudy/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/devstudy/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/devstudy/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/devstudy/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/devstudy/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/devstudy/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/devstudy/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/devstudy/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/devstudy/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/devstudy/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/devstudy/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/devstudy/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/devstudy/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/devstudy/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "7fbb92eee3c1b90d9df1756a03f0883a",
+    "url": "/devstudy/static/media/discordd18.7fbb92ee.jpg"
+  }
+]);
